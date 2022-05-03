ba55fb1098451b6837260deb3f32323a52c53242
karthikkc24 <karthikkc1998@gmail.com>
2022-05-03 11:53:05 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const app = express();
 
+const appInsights = require("applicationinsights");
+appInsights.setup("61da8814-911d-4e58-9cc2-994409fba955");
+appInsights.start();
+
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
 const contentApiUrl = process.env.CONTENT_API_URL || "http://localhost:3001";
 
